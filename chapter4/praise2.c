7dd71cf2d18a52b7d77c1d4b3f19b84eff9ae7c5
Lei Chen <lei8c8@gmail.com>
2020-05-25 14:52:05 -0400

    finished learning chapter4

---
+++ chapter4/praise2.c
@@ -0,0 +1,17 @@
+/* praise2.c */
+#include <stdio.h>
+#include <string.h>
+#define PRAISE "What a super marvelous name!"
+
+int main(void)
+{
+    char name[40];
+
+    printf("What's your first name?\n");
+    scanf("%s", name);
+    printf("Hello, %s. %s\n", name, PRAISE);
+    printf("Your name of %lu letters occupies %lu memory cells.\n", strlen(name), sizeof name);
+    printf("The praise of praise has %lu letters ", strlen(PRAISE));
+    printf("and occupies %lu memory cells.\n", sizeof PRAISE);
+    return 0;
+}
